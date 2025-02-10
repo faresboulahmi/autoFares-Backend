47398a92b5676fd92f9785c7cda4827683824b31
fares <132559795+faresboulahmi@users.noreply.github.com>
2025-02-10 09:10:51 +0100

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken , verifyTokenTFP } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-router.get('/listings/:id' , getUserListings)
+router.get('/listings/:id' , verifyTokenTFP , getUserListings)
 router.get('/:id', verifyToken, getUser)
 
 export default router;
